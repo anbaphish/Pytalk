9668c48823d2cd0472f95f758657823239e983c4
Phish <78632176+anbaphish@users.noreply.github.com>
2022-09-30 23:34:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #basic proof of concept
 import random
 words = { 
-    "adj": ["good", "sad", "boring", "loud", "fun"],
-    "topics": ["school", "math", "sciene", "work", "exercise", "music"]
+    "adj": ["good","sad","boring","loud","fun"],
+    "topics": ["school","math","sciene","work","exercise","music"]
 }
 
 
@@ -11,11 +11,9 @@ words = {
 #topics = words["topics"]
 
 
-speak = "I think", random.choice(words["topics"]), " is ", random.choice(words["adj"])
+speak = (f"I think",random.choice(words["topics"]),'is',random.choice(words["adj"]))
 print(speak)
     
-def randchoice(topic) : 
-    random.choice(list(words.topic()))
-    return
+
   
   #needs more work
