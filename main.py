18f026d77726b7525e7ea7ba4a0fd1575759f3b9
Phish <78632176+anbaphish@users.noreply.github.com>
2022-10-12 13:29:55 -0700

    Sentence uses list
    Will make it easier to dump to JSON, working on read and output with sentence list

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 import random
 import json
 
+
+
 with open('sample.json') as json_file:
    data = json.load(json_file) #loads JSON, use data as rep
+  
+sentSet = [(f"I think " + random.choice(data["topics"]) + ' is ' + random.choice(data["adj"]) ), 
+f"I love " + random.choice(data["topics"])
+          ]
 
-speak = (f"I think " + random.choice(data["topics"]) + ' is ' + random.choice(data["adj"]) )   
+
+speak = random.choice(sentSet)
 print(speak)
 
-#with open("sample.json", "w") as outfile:
-#    json.dump(words, outfile)
+#with open("list.json", "w") as outfile:
+ #  json.dump(sentSet, outfile)
   #opening a dictionary and dumping is best for JSON in format
